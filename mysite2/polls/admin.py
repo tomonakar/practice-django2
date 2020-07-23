7dca214fca07e92dae81c55a552046dbec6659d1
tomonakar <tomohisa.nak@gmail.com>
2020-07-23 16:40:21 +0900

    feat: 管理画面のquestionフィールドを並び替え

--- mysite2/polls/admin.py
+++ mysite2/polls/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
-admin.site.register(Question)
