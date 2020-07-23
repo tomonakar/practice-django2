eff1da06e0eedc2285c8b40e375c9f848ff5ca17
tomonakar <tomohisa.nak@gmail.com>
2020-07-23 17:07:59 +0900

    feat: 管理画面をカスタマイズ

--- mysite2/polls/admin.py
+++ mysite2/polls/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
+# admin.StackedInlineは積み重ねて表示
+# admin.TabularInlineは一行で表示
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
@@ -16,6 +18,12 @@ class QuestionAdmin(admin.ModelAdmin):
     # inles リストでクラスを追加すると外部キーでリレーションを貼ったオブジェクトを
     # 同じ画面で追加できるようになる
     inlines = [ChoiceInline]
+    # 表示項目を編集
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    # フィルタ機能を追加
+    list_filter = ['pub_date']
+    # 検索機能を追加
+    search_fields = ['question_text']
 
 
 admin.site.register(Question, QuestionAdmin)
