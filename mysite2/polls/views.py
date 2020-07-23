1ca3c530325dc67adb2b01cf2951e6ca461e09de
tomonakar <tomohisa.nak@gmail.com>
2020-07-23 12:02:13 +0900

    refactor: ジェネリックビューを使用

--- mysite2/polls/views.py
+++ mysite2/polls/views.py
@@ -1,18 +1,31 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
+
 from .models import Choice, Question
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    # template_name を使用しない場合は、デフォルトで以下が呼ばれる
+    # <app name>/<model name>_list.html
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+    # template_name を使用しない場合は、デフォルトで以下が呼ばれる
+    # <app name>/<model name>_detail.html
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
@@ -29,8 +42,3 @@ def vote(request, question_id):
         selected_choice.save()
         return HttpResponseRedirect(reverse(
             'polls:results', args=(question.id,)))
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
