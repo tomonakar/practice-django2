61659c529add802c83a3fd9fec9a129f1e4b772d
tomonakar <tomohisa.nak@gmail.com>
2020-07-23 16:48:57 +0900

    feat: StackedInlineクラスを継承して、リレーションのある関連オブジェクトを管理画面で追加

--- mysite2/polls/admin.py
+++ mysite2/polls/admin.py
@@ -2,12 +2,20 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
 
     fieldsets = [
         (None, {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date']}),
     ]
+    # inles リストでクラスを追加すると外部キーでリレーションを貼ったオブジェクトを
+    # 同じ画面で追加できるようになる
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
